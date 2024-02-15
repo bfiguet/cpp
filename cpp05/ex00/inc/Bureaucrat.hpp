8b6bc60f913c09e07eddf9e5b069fbde7e5d0306
bfiguet <75065714+bfiguet@users.noreply.github.com>
2024-02-15 12:12:18 +0100

    Add files via upload

---
+++ cpp05/ex00/inc/Bureaucrat.hpp
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bfiguet <bfiguet@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/15 09:48:53 by bfiguet           #+#    #+#             */
+/*   Updated: 2024/02/15 12:07:34 by bfiguet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+# include <iostream>
+# include <string>
+# include <exception>
+
+class Bureaucrat
+{
+
+private:
+	std::string	const		_name;
+	int						_grade;
+
+public:
+	Bureaucrat();
+	Bureaucrat(std::string name, int grade);
+	Bureaucrat(const Bureaucrat &src);
+	~Bureaucrat();
+
+	Bureaucrat&				operator=(const Bureaucrat &src);
+	std::string	const		getName()const;
+	int						getGrade()const;
+
+	void					setGrade(int grade);
+	void					increment();
+	void					decrement();
+
+	class GradeTooHighException : public std::exception {
+		public:
+			virtual const char* what() const throw();
+	};
+
+	class GradeTooLowException : public std::exception {
+		public:
+			virtual const char* what() const throw();
+	};
+	
+};
+
+std::ostream	&operator<<(std::ostream &o, Bureaucrat const &curr);
+
+#endif
