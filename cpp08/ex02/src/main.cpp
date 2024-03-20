85bfddc0dc92fa7989228980ab45d46953668eba
blandfi <blandinefiguet@gmail.com>
2024-03-20 17:22:32 +0100

    cpp08 ex02 OK

--- cpp08/ex02/src/main.cpp
+++ cpp08/ex02/src/main.cpp
@@ -6,13 +6,14 @@
 /*   By: bfiguet <bfiguet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 15:38:29 by bfiguet           #+#    #+#             */
-/*   Updated: 2024/03/20 13:26:32 by bfiguet          ###   ########.fr       */
+/*   Updated: 2024/03/20 17:21:28 by bfiguet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
 
 int main(void){
+
 	MutantStack<int> mstack;
 	mstack.push(5);
 	mstack.push(17);
@@ -22,7 +23,6 @@ int main(void){
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
-	//[...]
 	mstack.push(0);
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
@@ -30,9 +30,30 @@ int main(void){
 	--it;
 	while (it != ite)
 	{
-	std::cout << *it << std::endl;
-	++it;
+		std::cout << *it << std::endl;
+		++it;
 	}
 	std::stack<int> s(mstack);
+
+	std::cout << "LIST" << std::endl;
+	std::list<int> list;
+	list.push_front(5);
+	list.push_front(17);
+	std::cout << list.front() << std::endl;
+	list.pop_front();
+	std::cout << list.size() << std::endl;
+	list.push_back(3);
+	list.push_back(5);
+	list.push_back(737);
+	list.push_back(0);
+	std::list<int>::iterator it1 = list.begin();
+	std::list<int>::iterator ite1 = list.end();
+	++it1;
+	--it1;
+	while (it1 != ite1)
+	{
+		std::cout << *it1 << std::endl;
+		++it1;
+	}
 	return 0;
 }
