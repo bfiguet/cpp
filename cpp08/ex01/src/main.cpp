946de072dc96107ed88248cc0968bbe12fe637ce
bfiguet <75065714+bfiguet@users.noreply.github.com>
2024-04-16 12:48:50 +0200

    Update main.cpp

--- cpp08/ex01/src/main.cpp
+++ cpp08/ex01/src/main.cpp
@@ -14,18 +14,50 @@
 
 int main()
 {
-	Span sp = Span(5);
-	sp.addNumber(6);
-	sp.addNumber(3);
-	sp.addNumber(17);
-	sp.addNumber(9);
-	sp.addNumber(11);
-	std::cout << sp.shortestSpan() << std::endl;
-	std::cout << sp.longestSpan() << std::endl;
-
-	//Span sp1 = Span(3);
-	//sp1.addNums();
-	//std::cout << sp1.shortestSpan() << std::endl;
-	//std::cout << sp1.longestSpan() << std::endl;
+	Span sp = Span();
+	//Span sp = Span(5);
+	try
+	{
+		sp.addNumber(6);
+		sp.addNumber(3);
+		sp.addNumber(17);
+		sp.addNumber(9);
+		sp.addNumber(11);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	//sp.addNumber(68);
+	try
+	{
+		
+		std::cout << sp.shortestSpan() << std::endl;
+		std::cout << sp.longestSpan() << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
+	Span sp1 = Span(3);
+	try
+	{
+		sp1.addNums();
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
+	try
+	{
+		std::cout << sp1.shortestSpan() << std::endl;
+		std::cout << sp1.longestSpan() << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 	return 0;
 }
